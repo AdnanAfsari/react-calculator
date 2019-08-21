de2a8d0f54a35c95b63b8643bfeaebf8a0f24de0
Adnan Afsari <aarahul1991@gmail.com>
2019-08-21 14:34:47 +0600

    button components added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Display from '../Display/Display';
+import ButtonPanel from '../ButtonPanel/ButtonPanel'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Display />
+        <ButtonPanel />
       </header>
     </div>
   );
