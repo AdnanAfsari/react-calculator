86b83b1006ec225ae449693ae8e9b91681ebb29a
Adnan Afsari <aarahul1991@gmail.com>
2019-08-21 14:39:23 +0600

    buttonpanel component added

---
+++ src/components/ButtonPanel/ButtonPanel.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Button from '../Button/Button';
+
+const buttonPanel = () => {
+  return (
+    <div>
+      <h1>This is the ButtonPanel component</h1>
+      <h2>These are the buttons</h2>
+      <div ClassName = "group-1">
+        <Button name = "AC" />
+        <Button name = "+/-" />
+        <Button name = "%" />
+        <Button name = "+" />
+      </div>
+
+      <div ClassName = "group-2">
+        <Button name = "7" />
+        <Button name = "8" />
+        <Button name = "9" />
+        <Button name = "X" />
+      </div>
+
+
+      <div ClassName = "group-3">
+        <Button name = "4" />
+        <Button name = "5" />
+        <Button name = "6" />
+        <Button name = "-" />
+      </div>
+
+
+      <div ClassName = "group-4">
+        <Button name = "1" />
+        <Button name = "2" />
+        <Button name = "3" />
+        <Button name = "+" />
+      </div>
+
+
+      <div ClassName = "group-5">
+        <Button name = "0" />
+        <Button name = "." />
+        <Button name = "=" />
+      </div>
+
+
+    </div>
+  );
+}
+
+
+export default buttonPanel;
