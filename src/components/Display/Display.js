b4a96879d29513b6806f67d6844cb31cf0f5fab3
Adnan Afsari <aarahul1991@gmail.com>
2019-08-21 14:36:37 +0600

    display component added

---
+++ src/components/Display/Display.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const display = () => {
+  return (
+    <div>
+      <h1>This is the display components of the calculator</h1>
+    </div>
+  );
+}
+
+
+export default display;
