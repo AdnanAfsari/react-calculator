c0a3cb139a525daec6a3357a54a2282a7ba405ee
Adnan Afsari <aarahul1991@gmail.com>
2019-08-23 14:51:14 +0600

    stickler error fixed

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
+      <header className="App-header">  
       </header>
     </div>
   );
