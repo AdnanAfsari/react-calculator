28197346931d58461c341020615fdcc717a5c877
Adnan Afsari <aarahul1991@gmail.com>
2019-08-26 11:45:48 +0600

    added display props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-          <Display />
+          <Display value={"this is display panel"} />
           <ButtonPanel />
       </div>
     );
