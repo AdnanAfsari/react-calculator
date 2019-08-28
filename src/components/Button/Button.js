0131b7b3cf1b7e1cee05a39d49cf49cdcd15a964
Adnan Afsari <aarahul1991@gmail.com>
2019-08-29 02:28:08 +0600

    styling done

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 const button = (props) => {
+
+  const wide = props.wide ? "50%" : "25%"
+
+  const style = {
+    background: props.color,
+    width: wide
+  }
   return (
-    <div>
+    <div className = "button" style = {style}>
       <h3>{props.name}</h3>
     </div>
   );
--- src/components/ButtonPanel/ButtonPanel.js
+++ src/components/ButtonPanel/ButtonPanel.js
@@ -2,42 +2,44 @@ import React from 'react';
 import Button from '../Button/Button';
 
 const buttonPanel = () => {
+
+
   return (
     <div>
-      <div className = "group-1">
-        <Button name = "AC" />
-        <Button name = "+/-" />
-        <Button name = "%" />
-        <Button name = "+" />
+      <div className = "group group-1">
+        <Button name = "AC" color = "#E0E0E0" />
+        <Button name = "+/-" color = "#E0E0E0" />
+        <Button name = "%" color = "#E0E0E0" />
+        <Button name = "/" />
       </div>
 
-      <div className = "group-2">
-        <Button name = "7" />
-        <Button name = "8" />
-        <Button name = "9" />
+      <div className = "group group-2">
+        <Button name = "7" color = "#E0E0E0" />
+        <Button name = "8" color = "#E0E0E0" />
+        <Button name = "9" color = "#E0E0E0" />
         <Button name = "X" />
       </div>
 
 
-      <div className = "group-3">
-        <Button name = "4" />
-        <Button name = "5" />
-        <Button name = "6" />
+      <div className = "group group-3">
+        <Button name = "4" color = "#E0E0E0" />
+        <Button name = "5" color = "#E0E0E0" />
+        <Button name = "6" color = "#E0E0E0" />
         <Button name = "-" />
       </div>
 
 
-      <div className = "group-4">
-        <Button name = "1" />
-        <Button name = "2" />
-        <Button name = "3" />
+      <div className = "group group-4">
+        <Button name = "1" color = "#E0E0E0" />
+        <Button name = "2" color = "#E0E0E0" />
+        <Button name = "3" color = "#E0E0E0" />
         <Button name = "+" />
       </div>
 
 
-      <div className = "group-5">
-        <Button name = "0" />
-        <Button name = "." />
+      <div className = "group group-5">
+        <Button name = "0" color = "#E0E0E0" wide = "true" />
+        <Button name = "." color = "#E0E0E0" />
         <Button name = "=" />
       </div>
 
